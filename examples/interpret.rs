23dd1d2f7ff31fb5068d4c2a9180121454f9989c
Svyatoslav Nikolsky <svyatonik@gmail.com>
2017-05-10 17:06:09 +0300

    lost save

--- examples/interpret.rs
+++ examples/interpret.rs
@@ -6,7 +6,7 @@ use parity_wasm::ModuleInstanceInterface;
 
 fn main() {
     let args: Vec<_> = args().collect();
-    if args.len() != 2 {
+    if args.len() != 3 {
         println!("Usage: {} <wasm file> <arg>", args[0]);
         println!("    wasm file should contain exported `_call` function with single I32 argument");
         return;
@@ -15,6 +15,6 @@ fn main() {
     let program = parity_wasm::ProgramInstance::new().expect("Failed to load program");
     let module = parity_wasm::deserialize_file(&args[1]).expect("Failed to load module");
     let module = program.add_module("main", module).expect("Failed to initialize module");
-    let argument: i32 = args[1].parse().expect("Integer argument required");
+    let argument: i32 = args[2].parse().expect("Integer argument required");
     println!("Result: {:?}", module.execute_export("_call", vec![parity_wasm::RuntimeValue::I32(argument)]));
 }

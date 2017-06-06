b5603685ed5f1f720109cca62154447c9dc36de9
NikVolf <nikvolf@gmail.com>
2017-06-06 11:54:15 +0300

    remove testsuite link

--- spec/src/fixtures.rs
+++ spec/src/fixtures.rs
@@ -15,4 +15,3 @@ run_test!("f64_bitwise", wasm_f64_bitwise);
 run_test!("forward", wasm_address);
 run_test!("i32", wasm_i32);
 run_test!("i64", wasm_i64);
-run_test!("endianness", wasm_endianness);

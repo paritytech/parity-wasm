351e7ab45c1138e2f91d4ebd411da658e61c7ec3
fro <frol.rage@gmail.com>
2017-08-17 16:53:37 +0300

    comments added to the invoke example

--- examples/invoke.rs
+++ examples/invoke.rs
@@ -15,6 +15,7 @@ fn main() {
     let func_name = &args[2];
     let (_, program_args) = args.split_at(3);
 
+    // Intrepreter initialization
     let program = parity_wasm::DefaultProgramInstance::with_env_params(
         interpreter::EnvParams {
             total_stack: 128*1024,
@@ -24,11 +25,17 @@ fn main() {
     ).expect("Program instance to load");
 
     let module = parity_wasm::deserialize_file(&args[1]).expect("File to be deserialized");
+
+    // Extracts call arguments from command-line arguments
     let execution_params = {
+        // Export section has an entry with a func_name with an index inside a module
         let export_section = module.export_section().expect("No export section found");
+        // It's a section where function to be found in
         let function_section = module.function_section().expect("No function section found");
+        // Type section stores function types
         let type_section = module.type_section().expect("No type section found");
-        
+
+        //
         let found_entry = export_section.entries().iter()
             .find(|entry| func_name == entry.field()).expect(&format!("No export with name {} found", func_name));
 
@@ -37,8 +44,9 @@ fn main() {
             &Internal::Function(index) => index as usize,
             _ => panic!("Founded export is not a function"),
         };
+        // Counts import section entries
         let import_section_len: usize = match module.import_section() {
-            Some(import) => 
+            Some(import) =>
                 import.entries().iter().filter(|entry| match entry.external() {
                     &External::Function(_) => true,
                     _ => false,
@@ -46,12 +54,16 @@ fn main() {
             None => 0,
         };
 
+        // Calculates a function index within function section
         let function_index_in_section = function_index - import_section_len;
+
+        // Type a type ref (an index inside of types section of module)
         let func_type_ref: usize = function_section.entries()[function_index_in_section].type_ref() as usize;
         let function_type: &FunctionType = match &type_section.types()[func_type_ref] {
             &Type::Function(ref func_type) => func_type,
         };
 
+        // Parses arguments and constructs runtime values in correspondence of their types
         let args: Vec<RuntimeValue> = function_type.params().iter().enumerate().map(|(i, value)| match value {
             &ValueType::I32 => RuntimeValue::I32(program_args[i].parse::<i32>().expect(&format!("Can't parse arg #{} as i32", program_args[i]))),
             &ValueType::I64 => RuntimeValue::I64(program_args[i].parse::<i64>().expect(&format!("Can't parse arg #{} as i64", program_args[i]))),
@@ -61,7 +73,8 @@ fn main() {
 
         interpreter::ExecutionParams::from(args)
     };
-    
+
+    // Intialize deserialized module
     let module = program.add_module("main", module, None).expect("Failed to initialize module");
 
     println!("Result: {:?}", module.execute_export(func_name, execution_params).expect(""));

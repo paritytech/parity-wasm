03b890da40c827af992793cb47610661a90bad9c
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-01-20 01:47:56 +0300

    Use binaryen-rs for in-process wasm fuzzing

--- fuzz/fuzz_targets/deserialize.rs
+++ fuzz/fuzz_targets/deserialize.rs
@@ -2,54 +2,17 @@
 #[macro_use]
 extern crate libfuzzer_sys;
 extern crate parity_wasm;
-extern crate mktemp;
-
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
-use std::process::Command;
-
-fn wasm_opt() -> PathBuf {
-    let bin = PathBuf::from(env!("OUT_DIR")).join("bin").join("wasm-opt");
-    assert!(
-        bin.exists(),
-        format!(
-            "could not find wasm-opt at location installed by build.rs: {:?}",
-            wasm_opt()
-        )
-    );
-    bin
-}
+extern crate binaryen;
 
 fuzz_target!(|data: &[u8]| {
-    let seed = mktemp::Temp::new_file().expect("mktemp file to store fuzzer input");
-    let mut seedfile =
-        File::create(seed.as_ref()).expect("open temporary file for writing to store fuzzer input");
-    seedfile.write_all(data).expect(
-        "write fuzzer input to temporary file",
-    );
-    seedfile.flush().expect(
-        "flush fuzzer input to temporary file before starting wasm-opt",
-    );
+    let binaryen_module = binaryen::tools::translate_to_fuzz(data);
 
-    let wasm = mktemp::Temp::new_file().expect("mktemp file to store wasm-opt output");
-    let opt_fuzz = Command::new(wasm_opt())
-        .arg("--translate-to-fuzz")
-        .arg(seed.as_ref())
-        .arg("-o")
-        .arg(wasm.as_ref())
-        .output()
-        .expect("execute wasm-opt installed by build.rs");
+    // enable binaryen's validation if in doubt.
+    // assert!(binaryen_module.is_valid());
 
-    assert!(
-        opt_fuzz.status.success(),
-        format!(
-            "wasm-opt failed with: {}",
-            String::from_utf8_lossy(&opt_fuzz.stderr)
-        )
-    );
+    let wasm = binaryen_module.write();
 
-    let _module: parity_wasm::elements::Module = parity_wasm::deserialize_file(wasm.as_ref())
+    let _module: parity_wasm::elements::Module = parity_wasm::deserialize_buffer(&wasm)
         .expect(
             "deserialize output of wasm-opt, indicating possible bug in deserializer",
         );

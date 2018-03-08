bfe87d89df3c26852e98db7b30bc31d607172a5f
Arnavion <arnavion@gmail.com>
2018-03-07 21:24:43 -0800

    Fixed reloc section doc comments and removed prefix from entry type constants.

--- src/elements/reloc_section.rs
+++ src/elements/reloc_section.rs
@@ -2,28 +2,28 @@ use std::io::{Read, Write};
 
 use super::{CountedList, CountedListWriter, CountedWriter, Deserialize, Error, Serialize, VarInt32, VarUint32, VarUint7};
 
-const R_WEBASSEMBLY_FUNCTION_INDEX_LEB: u8 = 0;
-const R_WEBASSEMBLY_TABLE_INDEX_SLEB: u8 = 1;
-const R_WEBASSEMBLY_TABLE_INDEX_I32: u8 = 2;
-const R_WEBASSEMBLY_MEMORY_ADDR_LEB: u8 = 3;
-const R_WEBASSEMBLY_MEMORY_ADDR_SLEB: u8 = 4;
-const R_WEBASSEMBLY_MEMORY_ADDR_I32: u8 = 5;
-const R_WEBASSEMBLY_TYPE_INDEX_LEB: u8 = 6;
-const R_WEBASSEMBLY_GLOBAL_INDEX_LEB: u8 = 7;
+const FUNCTION_INDEX_LEB: u8 = 0;
+const TABLE_INDEX_SLEB: u8 = 1;
+const TABLE_INDEX_I32: u8 = 2;
+const MEMORY_ADDR_LEB: u8 = 3;
+const MEMORY_ADDR_SLEB: u8 = 4;
+const MEMORY_ADDR_I32: u8 = 5;
+const TYPE_INDEX_LEB: u8 = 6;
+const GLOBAL_INDEX_LEB: u8 = 7;
 
 /// Relocation information.
 #[derive(Clone, Debug)]
 pub struct RelocSection {
-	// Name of this section.
+	/// Name of this section.
 	name: String,
 
-	// ID of the section containing the relocations described in this section.
+	/// ID of the section containing the relocations described in this section.
 	section_id: u32,
 
-	// Name of the section containing the relocations described in this section. Only set if section_id is 0.
+	/// Name of the section containing the relocations described in this section. Only set if section_id is 0.
 	relocation_section_name: Option<String>,
 
-	// Relocation entries.
+	/// Relocation entries.
 	entries: Vec<RelocationEntry>,
 }
 
@@ -33,6 +33,7 @@ impl RelocSection {
 		rdr: &mut R,
 	) -> Result<Self, Error> {
 		let section_id = VarUint32::deserialize(rdr)?.into();
+
 		let relocation_section_name =
 			if section_id == 0 {
 				Some(String::deserialize(rdr)?)
@@ -57,6 +58,7 @@ impl Serialize for RelocSection {
 
 	fn serialize<W: Write>(self, wtr: &mut W) -> Result<(), Error> {
 		let mut counted_writer = CountedWriter::new(wtr);
+
 		self.name.serialize(&mut counted_writer)?;
 
 		VarUint32::from(self.section_id).serialize(&mut counted_writer)?;
@@ -77,52 +79,55 @@ impl Serialize for RelocSection {
 /// Relocation entry.
 #[derive(Clone, Debug)]
 pub enum RelocationEntry {
-	// Function index.
+	/// Function index.
 	FunctionIndexLeb {
+		/// Offset of the value to rewrite.
 		offset: u32,
+
+		/// Index of the function symbol in the symbol table.
 		index: u32,
 	},
 
-	// Function table index.
+	/// Function table index.
 	TableIndexSleb {
 		offset: u32,
 		index: u32,
 	},
 
-	// Function table index.
+	/// Function table index.
 	TableIndexI32 {
 		offset: u32,
 		index: u32,
 	},
 
-	// Linear memory index.
+	/// Linear memory index.
 	MemoryAddressLeb {
 		offset: u32,
 		index: u32,
 		addend: i32,
 	},
 
-	// Linear memory index.
+	/// Linear memory index.
 	MemoryAddressSleb {
 		offset: u32,
 		index: u32,
 		addend: i32,
 	},
 
-	// Linear memory index.
+	/// Linear memory index.
 	MemoryAddressI32 {
 		offset: u32,
 		index: u32,
 		addend: i32,
 	},
 
-	// Type table index.
+	/// Type table index.
 	TypeIndexLeb {
 		offset: u32,
 		index: u32,
 	},
 
-	// Global index.
+	/// Global index.
 	GlobalIndexLeb {
 		offset: u32,
 		index: u32,
@@ -134,45 +139,45 @@ impl Deserialize for RelocationEntry {
 
 	fn deserialize<R: Read>(rdr: &mut R) -> Result<Self, Self::Error> {
 		match VarUint7::deserialize(rdr)?.into() {
-			R_WEBASSEMBLY_FUNCTION_INDEX_LEB => Ok(RelocationEntry::FunctionIndexLeb {
+			FUNCTION_INDEX_LEB => Ok(RelocationEntry::FunctionIndexLeb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_TABLE_INDEX_SLEB => Ok(RelocationEntry::TableIndexSleb {
+			TABLE_INDEX_SLEB => Ok(RelocationEntry::TableIndexSleb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_TABLE_INDEX_I32 => Ok(RelocationEntry::TableIndexI32 {
+			TABLE_INDEX_I32 => Ok(RelocationEntry::TableIndexI32 {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_MEMORY_ADDR_LEB => Ok(RelocationEntry::MemoryAddressLeb {
-				offset: VarUint32::deserialize(rdr)?.into(),
-				index: VarUint32::deserialize(rdr)?.into(),
-				addend: VarInt32::deserialize(rdr)?.into(),
-			}),
-
-			R_WEBASSEMBLY_MEMORY_ADDR_SLEB => Ok(RelocationEntry::MemoryAddressSleb {
+			MEMORY_ADDR_LEB => Ok(RelocationEntry::MemoryAddressLeb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 				addend: VarInt32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_MEMORY_ADDR_I32 => Ok(RelocationEntry::MemoryAddressI32 {
+			MEMORY_ADDR_SLEB => Ok(RelocationEntry::MemoryAddressSleb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 				addend: VarInt32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_TYPE_INDEX_LEB => Ok(RelocationEntry::TypeIndexLeb {
+			MEMORY_ADDR_I32 => Ok(RelocationEntry::MemoryAddressI32 {
+				offset: VarUint32::deserialize(rdr)?.into(),
+				index: VarUint32::deserialize(rdr)?.into(),
+				addend: VarInt32::deserialize(rdr)?.into(),
+			}),
+
+			TYPE_INDEX_LEB => Ok(RelocationEntry::TypeIndexLeb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 			}),
 
-			R_WEBASSEMBLY_GLOBAL_INDEX_LEB => Ok(RelocationEntry::GlobalIndexLeb {
+			GLOBAL_INDEX_LEB => Ok(RelocationEntry::GlobalIndexLeb {
 				offset: VarUint32::deserialize(rdr)?.into(),
 				index: VarUint32::deserialize(rdr)?.into(),
 			}),
@@ -188,52 +193,52 @@ impl Serialize for RelocationEntry {
 	fn serialize<W: Write>(self, wtr: &mut W) -> Result<(), Error> {
 		match self {
 			RelocationEntry::FunctionIndexLeb { offset, index } => {
-				VarUint7::from(R_WEBASSEMBLY_FUNCTION_INDEX_LEB).serialize(wtr)?;
+				VarUint7::from(FUNCTION_INDEX_LEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 			},
 
 			RelocationEntry::TableIndexSleb { offset, index } => {
-				VarUint7::from(R_WEBASSEMBLY_TABLE_INDEX_SLEB).serialize(wtr)?;
+				VarUint7::from(TABLE_INDEX_SLEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 			},
 
 			RelocationEntry::TableIndexI32 { offset, index } => {
-				VarUint7::from(R_WEBASSEMBLY_TABLE_INDEX_I32).serialize(wtr)?;
+				VarUint7::from(TABLE_INDEX_I32).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 			},
 
 			RelocationEntry::MemoryAddressLeb { offset, index, addend } => {
-				VarUint7::from(R_WEBASSEMBLY_MEMORY_ADDR_LEB).serialize(wtr)?;
+				VarUint7::from(MEMORY_ADDR_LEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 				VarInt32::from(addend).serialize(wtr)?;
 			},
 
 			RelocationEntry::MemoryAddressSleb { offset, index, addend } => {
-				VarUint7::from(R_WEBASSEMBLY_MEMORY_ADDR_SLEB).serialize(wtr)?;
+				VarUint7::from(MEMORY_ADDR_SLEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 				VarInt32::from(addend).serialize(wtr)?;
 			},
 
 			RelocationEntry::MemoryAddressI32 { offset, index, addend } => {
-				VarUint7::from(R_WEBASSEMBLY_MEMORY_ADDR_I32).serialize(wtr)?;
+				VarUint7::from(MEMORY_ADDR_I32).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 				VarInt32::from(addend).serialize(wtr)?;
 			},
 
 			RelocationEntry::TypeIndexLeb { offset, index } => {
-				VarUint7::from(R_WEBASSEMBLY_TYPE_INDEX_LEB).serialize(wtr)?;
+				VarUint7::from(TYPE_INDEX_LEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 			},
 
 			RelocationEntry::GlobalIndexLeb { offset, index } => {
-				VarUint7::from(R_WEBASSEMBLY_GLOBAL_INDEX_LEB).serialize(wtr)?;
+				VarUint7::from(GLOBAL_INDEX_LEB).serialize(wtr)?;
 				VarUint32::from(offset).serialize(wtr)?;
 				VarUint32::from(index).serialize(wtr)?;
 			},

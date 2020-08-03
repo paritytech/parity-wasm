7f171473a085ab1fdd8487bbdf3b70b84fd7d312
Travis CI User <travis@example.org>
2020-08-03 12:53:31 +0000

    Update documentation

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parity_wasm"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"parity_wasm/elements/enum.Error.html\" title=\"enum parity_wasm::elements::Error\">Error</a>","synthetic":false,"types":["parity_wasm::elements::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["parity_wasm"] = {"name":"","dirs":[{"name":"builder","files":["code.rs","data.rs","export.rs","global.rs","import.rs","invoke.rs","memory.rs","misc.rs","mod.rs","module.rs","table.rs"]},{"name":"elements","files":["export_entry.rs","func.rs","global_entry.rs","import_entry.rs","index_map.rs","mod.rs","module.rs","name_section.rs","ops.rs","primitives.rs","reloc_section.rs","section.rs","segment.rs","types.rs"]}],"files":["io.rs","lib.rs"]};
+createSourceSidebar();

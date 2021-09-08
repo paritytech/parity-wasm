ba37ccc1e573d1f0d4ca6b77604442df0a257294
Alexander Theißen <alex.theissen@me.com>
2021-09-08 10:58:33 +0200

    Blacklist tests that fail for known reasons

--- testsuite/src/lib.rs
+++ testsuite/src/lib.rs
@@ -1,8 +1,30 @@
 #![cfg(test)]
 
+use std::ffi::OsStr;
+
 mod run;
+
+const BASIC_BLACKLIST: [&str; 8] = [
+	// those use unsupported i32_trunc_sat_* instructions
+	"binary-leb128.wast",
+	"conversions.wast",
+	// those use multi-value for blocks which is unsupported
+	"block.wast",
+	"func.wast",
+	"if.wast",
+	"loop.wast",
+	"fac.wast",
+	"br.wast",
+];
 
 #[test_generator::test_resources("testsuite/spec/*.wast")]
 fn basic(path: &str) {
-	run::check(path);
+	let blacklisted = std::path::Path::new(path)
+		.file_name()
+		.map(|file| BASIC_BLACKLIST.iter().any(|black| OsStr::new(black) == file))
+		.unwrap_or(false);
+
+	if !blacklisted {
+		run::check(path);
+	}
 }
